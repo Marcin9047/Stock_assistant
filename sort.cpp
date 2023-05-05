01bc7983ef8b78208efcc2193b83803d7feb77d9
aprzybor <01171594@pw.edu.pl>
2023-05-06 00:24:38 +0200

    basic draw of sort class

---
+++ sort.cpp
@@ -0,0 +1,33 @@
+#include <data.h>
+
+sort::sort(int capital, char attitude)
+{
+    float wsp=0;
+    if(attitude=='')
+    {
+        //api request
+        for(//every brand)
+        {
+
+            data data();
+            //algorytmy wyliczające czy firma spełnia zalozenia
+            float new_wsp;
+            if(new_wsp>wsp)
+            {
+                brand_name = data.get_stock()
+                wsp = new_wsp;
+            }
+        }
+    }
+    else
+    {
+        //else api request
+
+    }
+
+}
+
+brand sort::best_match()
+{
+    return brand(char brand_name);
+}
