a4f6a80b962f851419716914ab0dac99fc8eb3fd
aprzybor <01171594@pw.edu.pl>
2023-05-20 18:01:39 +0200

    added subclass to manage diffrent json structure

--- src/sort/JsonParser.hpp
+++ src/sort/JsonParser.hpp
@@ -182,3 +182,105 @@ public:
     }
 
 };
+
+
+class NASDAQ_pars :public JsonParser
+{
+public:
+
+    std::vector<DataPoint> parse_NASDAQ(const std::string& jsonStr)
+    {
+        std::vector<DataPoint> dataPoints;
+
+        try {
+            json jsonData = json::parse(jsonStr);
+
+            if (jsonData["Response"] == "Success") {//to be changed
+                json data = jsonData["Data"]["Data"];//to be changed
+
+                for (const auto& item : data) {
+                    DataPoint dp;
+                    dp.time = item["time"].dump();
+                    dp.open = item["open"].get<double>();
+                    dp.high = item["high"].get<double>();
+                    dp.low = item["low"].get<double>();
+                    dp.volumeFrom = item["volumefrom"].get<double>();
+                    dp.volumeTo = item["volumeto"].get<double>();
+                    dp.close = item["close"].get<double>();
+
+                    dataPoints.push_back(dp);
+                }
+            }
+        } catch (const json::exception& e) {
+            std::cerr << "JSON parsing error: " << e.what() << std::endl;
+        }
+
+        return dataPoints;
+    }
+
+    std::list<std::string> getTimeList(const std::vector<JsonParser::DataPoint>& dataPoints)
+    {
+        JsonParser::getTimeList(dataPoints);
+    }
+
+    std::list<double> getOpenList(const std::vector<DataPoint>& dataPoints) {
+        JsonParser::getOpenList(dataPoints);
+
+    }
+
+    std::list<double> getHighList(const std::vector<DataPoint>& dataPoints) {
+        JsonParser::getHighList(dataPoints);
+
+    }
+
+    std::list<double> getLowList(const std::vector<DataPoint>& dataPoints) {
+        JsonParser::getLowList(dataPoints);
+
+    }
+
+    std::list<double> getVolumeFromList(const std::vector<DataPoint>& dataPoints) {
+        JsonParser::getVolumeFromList(dataPoints);
+
+    }
+
+    std::list<double> getVolumeToList(const std::vector<DataPoint>& dataPoints) {
+        JsonParser::getVolumeToList(dataPoints);
+
+    }
+
+    std::list<double> getCloseList(const std::vector<DataPoint>& dataPoints) {
+        JsonParser::getCloseList(dataPoints);
+
+    }
+
+    std::vector<double> getCloseVector(const std::vector<DataPoint>& dataPoints) {
+        JsonParser::getCloseVector(dataPoints);
+
+    }
+
+    std::vector<double> getOpenVector(const std::vector<DataPoint>& dataPoints) {
+        JsonParser::getOpenVector(dataPoints);
+
+    }
+
+    std::vector<double> getHighVector(const std::vector<DataPoint>& dataPoints) {
+        JsonParser::getHighVector(dataPoints);
+
+    }
+
+    std::vector<double> getLowVector(const std::vector<DataPoint>& dataPoints) {
+        JsonParser::getLowVector(dataPoints);
+
+    }
+
+    std::vector<double> getVolumeFromVector(const std::vector<DataPoint>& dataPoints) {
+        JsonParser::getVolumeFromVector(dataPoints);
+
+    }
+
+    std::vector<double> getVolumeToVector(const std::vector<DataPoint>& dataPoints) {
+        JsonParser::getVolumeToVector(dataPoints);
+
+    }
+
+};
