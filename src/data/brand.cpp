e9dedc51658e20b2e4b0efefbdfafebab269d69e
Marcin Połosak <marcin.polosak@wp.pl>
2023-05-27 18:11:52 +0200

    Moved files for the suitable folders

---
+++ src/data/brand.cpp
@@ -0,0 +1,40 @@
+#include "brand.h"
+#include <iostream>
+#include <string>
+
+
+
+// brand::brand(std::string name)
+// {
+//     brand_name = name;
+// };
+
+// std::string brand::get_brand()
+// {
+//     return brand_name;
+// }
+
+// void brand::clear_before(int time)
+// {
+
+// };
+
+// void operator<<(brand& b1, data<class T>& d1)
+// {
+//     b1.history.push_back(d1);
+// };
+
+// float brand::last_value()
+// {
+//     return 0;
+// };
+
+// float brand::last_read_time()
+// {
+//     return 0;
+// };
+
+// bool brand::is_rising()
+// {
+//     return true;
+// }
---
+++ src/data/data.cpp
@@ -0,0 +1,23 @@
+#include "data.h"
+#include <string>
+
+
+// <class T> data<class T>::get_time()
+// {
+//     return read_time;
+// };
+
+// float data<class T>::get_high_value()
+// {
+//     return high_value;
+// };
+
+// float data<class T>::get_low_value()
+// {
+//     return low_value;
+// };
+
+int nasdaq_data::converted_time()
+{
+    return 0;
+}
